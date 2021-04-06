4c29533047b128ff227d9036bacad7cd57820b7d
umahato <47037548+umahato@users.noreply.github.com>
2021-04-06 22:58:38 +0530

    Create 5_ListComprehension.py

---
+++ 5_ListComprehension.py
@@ -0,0 +1,20 @@
+#   List Comprehensions are unique way of quickly creating a list with Python.
+#   If you find yourself usig a a for loop along with .append() to create a list,
+#   List Comprehensions are good alternative
+
+'''
+mystring = 'hello'
+mylist = []
+
+for letter in mystring:
+    mylist.append(letter)
+
+print(mylist)
+
+
+'''
+
+# alternate way.
+mystring = 'hello'
+mylist = [letter for letter in mystring]
+print(mylist)
